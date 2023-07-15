f0b564519e7ddc124d3f546c45ca259263ceb0cd
Maksymilian Pietras <115542149+czisCierpiacy@users.noreply.github.com>
2023-07-15 14:58:37 +0200

    Reading items headres from steam site

---
+++ src/MainSystem.java
@@ -0,0 +1,51 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainSystem {
+
+    public static final int NUMBER_OF_PAGES = 44215;
+    public static final String SORTING = "_popular_desc";
+    public static final int ITEMS_ON_PAGE = 10;
+    public static final int ONE_NEXT_INDEX = 1;
+
+    public static void main(String[] args) throws IOException {
+
+        String staticUrlPart = "https://steamcommunity.com/market/search?q=#";
+
+        for (int currentPageIndex = 0; currentPageIndex < NUMBER_OF_PAGES; currentPageIndex++){
+            URL currentItemUrl = new URL(staticUrlPart + currentPageIndex + SORTING);
+            HttpURLConnection pageConnection = (HttpURLConnection) currentItemUrl.openConnection();
+            pageConnection.setRequestMethod("GET");
+            if (pageConnection.getResponseCode() != 200){
+                System.out.println("Odrzucono połączenie");
+            } else {
+                System.out.println("Połączono");
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(pageConnection.getInputStream()));
+            StringBuilder content = new StringBuilder();
+            StringBuilder pageHtml = new StringBuilder();
+            String singleHtmlLine;
+            while ((singleHtmlLine = reader.readLine()) != null){
+                pageHtml.append(singleHtmlLine);
+            }
+            List <Integer> itemsDataStartIndexes = new ArrayList<>();
+            List <Integer> itemsDataEndIndexes = new ArrayList<>();
+            for (int currentItemIndex = 0; currentItemIndex < ITEMS_ON_PAGE; currentItemIndex++){
+
+                itemsDataStartIndexes.add(pageHtml.indexOf("\"result_" + currentItemIndex));
+                String itemDataHtmlPart = pageHtml.substring(itemsDataStartIndexes.get(currentItemIndex));
+                itemsDataEndIndexes.add(itemDataHtmlPart.indexOf(">") + itemsDataStartIndexes.get(currentItemIndex) + ONE_NEXT_INDEX);
+            }
+            for (int i = 0; i < 10; i++) {
+                System.out.println(pageHtml.substring(itemsDataStartIndexes.get(i), itemsDataEndIndexes.get(i)));
+            }
+        }
+
+    }
+}
