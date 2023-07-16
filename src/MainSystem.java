a745c3b0e0a277d1d591ec2b60fabd3de80fe5f3
Maksymilian Pietras <240774@edu.p.lodz.pl>
2023-07-16 13:55:30 +0200

    Getting all items headers (all pages)

--- src/MainSystem.java
+++ src/MainSystem.java
@@ -4,22 +4,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainSystem {
 
     public static final int NUMBER_OF_PAGES = 44215;
-    public static final String SORTING = "_popular_desc";
+    public static final String SORTING = "&count=10&search_descriptions=0&sort_column=popular&sort_dir=desc";
     public static final int ITEMS_ON_PAGE = 10;
     public static final int ONE_NEXT_INDEX = 1;
-
+    //todo nie da sie robic tego tak na linkach tezeba symulować klikanie przycisku
     public static void main(String[] args) throws IOException {
 
-        String staticUrlPart = "https://steamcommunity.com/market/search?q=#";
+        String staticUrlPart = "https://steamcommunity.com/market/search/render/?query=&start=";
 
-        for (int currentPageIndex = 0; currentPageIndex < NUMBER_OF_PAGES; currentPageIndex++){
-            URL currentItemUrl = new URL(staticUrlPart + currentPageIndex + SORTING);
+        for (int currentPageIndex = 0; currentPageIndex <= NUMBER_OF_PAGES; currentPageIndex+=10){
+            String url = staticUrlPart + currentPageIndex + SORTING;
+
+            URL currentItemUrl = new URL(url);
             HttpURLConnection pageConnection = (HttpURLConnection) currentItemUrl.openConnection();
             pageConnection.setRequestMethod("GET");
             if (pageConnection.getResponseCode() != 200){
@@ -28,7 +32,6 @@ public class MainSystem {
                 System.out.println("Połączono");
             }
             BufferedReader reader = new BufferedReader(new InputStreamReader(pageConnection.getInputStream()));
-            StringBuilder content = new StringBuilder();
             StringBuilder pageHtml = new StringBuilder();
             String singleHtmlLine;
             while ((singleHtmlLine = reader.readLine()) != null){
@@ -45,6 +48,7 @@ public class MainSystem {
             for (int i = 0; i < 10; i++) {
                 System.out.println(pageHtml.substring(itemsDataStartIndexes.get(i), itemsDataEndIndexes.get(i)));
             }
+            reader.close();
         }
 
     }
