95661b158657af3f5c2a30746e39415e4605ebc6
Maksymilian Pietras <240774@edu.p.lodz.pl>
2023-07-30 18:44:31 +0200

    Refactor

--- src/main/java/com/example/steamofferswithgui/SteamOffersGui.java
+++ src/main/java/com/example/steamofferswithgui/SteamOffersGui.java
@@ -11,11 +11,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class SteamOffersGui extends Application {
+
+    public static final int SCENE_WIDTH = 1280;
+    public static final int SCENE_HEIGHT = 720;
+
     @Override
     public void start(Stage stage) throws IOException {
         GridPane mainPane = new GridPane();
         mainPane.setBackground(new Background(new BackgroundFill(Paint.valueOf("#171a21"), null, null)));
-        Scene scene = new Scene(mainPane, 1280, 720);
+        Scene scene = new Scene(mainPane, SCENE_WIDTH, SCENE_HEIGHT);
         stage.setTitle("Steam market offers by czisiasty - v1.0");
         stage.setScene(scene);
         stage.show();
