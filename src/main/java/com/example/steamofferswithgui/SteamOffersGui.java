dd14182a7c44f9e6139bfbc2904034ff7a7cb164
Maksymilian Pietras <240774@edu.p.lodz.pl>
2023-07-31 17:19:37 +0200

    First menu concept

--- src/main/java/com/example/steamofferswithgui/SteamOffersGui.java
+++ src/main/java/com/example/steamofferswithgui/SteamOffersGui.java
@@ -1,7 +1,12 @@
 package com.example.steamofferswithgui;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
@@ -14,15 +19,42 @@ public class SteamOffersGui extends Application {
 
     public static final int SCENE_WIDTH = 1280;
     public static final int SCENE_HEIGHT = 720;
+    public static final String BACKGROUND_COLOR_HEX = "#171a21";
+    public static final int LOGO_COLUMN_INDEX = 0;
+    public static final int LOGO_ROW_INDEX = 0;
+    public static final int COMBOBOX_COLUMN_INDEX = 0;
+    public static final int COMBOBOX_ROW_INDEX = 1;
 
     @Override
     public void start(Stage stage) throws IOException {
-        GridPane mainPane = new GridPane();
-        mainPane.setBackground(new Background(new BackgroundFill(Paint.valueOf("#171a21"), null, null)));
+        GridPane mainPane = initMenuPane();
+        mainPane.setBackground(new Background(new BackgroundFill(Paint.valueOf(BACKGROUND_COLOR_HEX), null, null)));
         Scene scene = new Scene(mainPane, SCENE_WIDTH, SCENE_HEIGHT);
         stage.setTitle("Steam market offers by czisiasty - v1.0");
         stage.setScene(scene);
         stage.show();
+        initMenu(mainPane);
+    }
+
+    private static GridPane initMenuPane() {
+        GridPane mainPane = new GridPane();
+        mainPane.setMinWidth(SCENE_WIDTH);
+        mainPane.setAlignment(Pos.TOP_CENTER);
+        return mainPane;
+    }
+
+    public void initMenu(GridPane mainPane){
+        ImageView steamOffersLogo = generateImage("/steamOffersImages/steamOffersLogo.png");
+        mainPane.add(steamOffersLogo, LOGO_COLUMN_INDEX, LOGO_ROW_INDEX);
+        ComboBox<String> possibleSearchOptions = new ComboBox<>();
+        possibleSearchOptions.setPromptText("Choose items to search");
+        possibleSearchOptions.getItems().add("★ StatTrack™");
+        mainPane.add(possibleSearchOptions, COMBOBOX_COLUMN_INDEX, COMBOBOX_ROW_INDEX);
+    }
+
+    public ImageView generateImage(String path){
+        Image image = new Image(getClass().getResourceAsStream(path));
+        return new ImageView(image);
     }
 
     public static void main(String[] args) {
